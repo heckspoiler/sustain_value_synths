e2eb475d447ffbe57aeaf0948795d8c45a2edff1
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-08-17 20:42:27 +0200

    api fetch doesn't work

--- components/index-body/News/NewsContainer.js
+++ components/index-body/News/NewsContainer.js
@@ -1,7 +1,24 @@
 import styles from "./NewsContainer.module.css";
 import NewsItem from "./NewsItem";
+import { fetchProducts } from "../lib/shopify";
+import { useEffect, useState } from "react";
 
 const NewsContainer = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const result = await fetchProducts();
+        setProducts(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <section className={styles["news-container"]}>
       <NewsItem />
