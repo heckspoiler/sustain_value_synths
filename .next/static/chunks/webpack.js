91c4539c7cdaea94421a06d4391a8a2033fa44d3
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-08-17 20:28:36 +0200

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c0e40ce9d52bd776"; }
+/******/ 		__webpack_require__.h = function() { return "f4e206af7178aa10"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c37daf26bcf23f56"; }
+/******/ 	__webpack_require__.h = function() { return "f4e206af7178aa10"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
