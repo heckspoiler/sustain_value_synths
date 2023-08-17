8e883f1d393462e8a87773f4a77544d9e54461db
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-08-17 12:42:15 +0200

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a4f2f1740f691e0"; }
+/******/ 		__webpack_require__.h = function() { return "ed924f9b063638cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <div className="bg-red-400 mt-10">Welcome to Next.js!</div>;
+  return (
+    <div className="bg-red-400 mt-10 h-20">
+      Welcome to Next.js!
+      <h1>Hello Motherfucker</h1>
+    </div>
+  );
 }
 
 export default Home;
